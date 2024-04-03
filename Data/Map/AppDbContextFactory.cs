10d2b7c99db2a9cbcb361c7c2a1575183d50b5cc
Bruna Romano <brunaclaudia468@gmail.com>
2024-04-03 09:27:57 -0300

    Conexao com DB e migrations

---
+++ Data/Map/AppDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+
+namespace API_Tarefas.Data.Map
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<SistemaTarefasDBContext>
+    {
+        public SistemaTarefasDBContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Criando o DbContextOptionsBuilder manualmente
+            var builder = new DbContextOptionsBuilder<SistemaTarefasDBContext>();
+            // cria a connection string. 
+            // requer a connectionstring no appsettings.json
+            var connectionString = configuration.GetConnectionString("DataBase");
+            builder.UseSqlServer(connectionString);
+
+            // Cria o contexto
+            return new SistemaTarefasDBContext(builder.Options);
+        }
+    }
+}
